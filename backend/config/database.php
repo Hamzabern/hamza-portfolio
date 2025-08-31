15cd0b0aac3d5cc9853474aa5c4b4046351b6bd7
Hamzabern <bernoussi_hameza@hotmail.fr>
2025-08-31 01:06:04 +0100

    feat (backend) : install laravel + tailwind + lirewire + Breeze

--- backend/config/database.php
+++ backend/config/database.php
@@ -44,6 +44,9 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
+            'engine' => 'InnoDB',
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
             'url' => env('DB_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
